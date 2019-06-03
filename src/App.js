328e0ba831fb236acaacd60355875bc97fd9d597
Nishant <nishant3@mail.usf.edu>
2019-06-03 17:03:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    
+    state = {toggle:false};
+    
+    
+     toggleDisplayText = () =>{
+         this.setState({toggle: !this.state.toggle});
+     }    
+
+    render(){
+        
+        return(
+           <div>
+            <h1>Hello Everyone!</h1>
+            <p>My name is Nishant Narula</p>
+            <p> Click on the button below to view more text</p>
+            {this.state.toggle ? (<div>
+             <p> Live your life according to your standards</p>
+             <p> Click on the below button to view less text</p>
+            <button onClick={this.toggleDisplayText}>Show Less:</button>
+            </div>
+         ):(
+        
+                 <div>
+               <button onClick={this.toggleDisplayText}>Show More:</button>
+            </div>
+        
+        )}
+            </div>
+            
+            
+            
+        
+        
+        
+        
+        
+        )
+        
+        
+    }
+    
+    
 }
+
 
 export default App;
